cf1ed370566989e6f0df728af389a42adc5047cb
sucholbluk <sucholbiaklukasz@gmail.com>
2024-10-29 17:36:54 +0100

    test: introduce simple example class and test files

---
+++ tests/example_test.py
@@ -0,0 +1,16 @@
+from pyndoc.ExampleClass import ExampleClass
+
+
+def test_example_class():
+    value = 4
+    example_instance = ExampleClass(value)
+
+    assert example_instance.num == value
+
+
+def test_always_passes():
+    assert True
+
+
+def test_always_fails():
+    assert False
