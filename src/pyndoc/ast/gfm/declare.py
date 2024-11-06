15760b800cd04e60e43d01cc377a70141f607f26
doaads <nowobart@gmail.com>
2024-11-06 19:40:41 +0100

    feat: swap around regex declaration key and value

--- src/pyndoc/ast/gfm/declare.py
+++ src/pyndoc/ast/gfm/declare.py
@@ -7,13 +7,9 @@ def declare_gfm():
     The "contents" group will be used as the block's contents
     """
     declared_blocks = {
-        # match a paragraph if no other block found
-        ast.Para: [None, None],
-        # match a markdown # Header
-        ast.Header: [r"^#{1,6} (?P<contents>.*$)", None],
-        # match *italic* text
-        ast.Emph: [r"(\*{1})(?P<contents>[^*\n]+?)(\*{1})", None],
-        ast.Strong: [r"(\*{2})(?P<contents>\*?[^\n]+?\*?)(\*{2})", None],
-        ast.Code: [r"(^|[^`])(`{1,2})(?P<contents>[^\n]+?)\2(?!`)", None],
+        r"^#{1,6} (?P<contents>.*$)": ast.Header,
+        r"(\*{1})(?P<contents>[^*\n]+?)(\*{1})": ast.Emph,
+        r"(\*{2})(?P<contents>\*?[^\n]+?\*?)(\*{2})": ast.Strong,
+        r"(^|[^`])(`{1,2})(?P<contents>[^\n]+?)\2(?!`)": ast.Code
     }
     return declared_blocks
