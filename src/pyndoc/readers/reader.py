ac257d7299c3394698ab2516b44ab423d09d52ff
Michal <01178574@pw.edu.pl>
2024-11-10 14:29:15 +0100

    fix: changed back to previous read file format

--- src/pyndoc/readers/reader.py
+++ src/pyndoc/readers/reader.py
@@ -13,34 +13,27 @@ class Reader():
 
         lang_module.assign_patterns()
 
+    # TODO: FINISH THIS!
     def _process_atom_block(self):
         """
-        Process an atom block (Str, Space, etc.)
+        process an atom block (Str, Space etc.)
         """
-        blocks = ast.decompose_text(self._token)
-
-        if self._context:
-            self._context[-1].contents.contents.extend(blocks)
-        else:
-            self._tree.extend(blocks)
-
-        self._token = ''
+        pass
 
     def _check_end(self):
         """
-        Check if the current context block has ended
+        check if the current context block has ended
         """
-        if not self._context:
-            return
-
         if self._context[-1]:
             end_match = self._context[-1].end(self._token)
             if not end_match:
                 return
 
+            # process token before the block-end
             self._token = self._token[:end_match.start()]
             self._process_atom_block()
 
+            # block is processed, move it to finished tree
             self._tree.append(self._context.pop())
 
     def _check_start(self):
@@ -50,15 +43,29 @@ class Reader():
         """
         for block in self._block_types:
             start_match = block.start(self._token)
+            if not start_match:
+                continue
+            self._token = self._token[:start_match.start()]
+            self._process_atom_block()
+
+            self._context = block(start_match)
+
 
     def process(self, char: str):
         """
-        Process a single character
+        Process a current token
+        taking into consideration the current context tree, check if
+        a new block has started or ended, process atom blocks
         """
         self._token += char
         self._check_end()
 
-    def read(self, filename: str) -> str:
+
+    def read(self, filename: str):
+        """
+        Open and read a file one character at a time,
+        then pass the character to tokenizer
+        """
         with open(filename, "r") as fp:
             contents = fp.read()
         for char in contents:
