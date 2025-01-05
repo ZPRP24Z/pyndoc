10127388cf2913d588576b872cce8357029a6864
doaads <nowobart@gmail.com>
2025-01-05 16:43:36 +0100

    chore: squash commits after merging origin/main to origin/table

--- src/pyndoc/readers/gfm/blocks.py
+++ src/pyndoc/readers/gfm/blocks.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import re
 from typing_extensions import Unpack
 from abc import ABC
@@ -163,3 +164,136 @@ class OrderedList(_GFMList, ast.OrderedList):
 
         self.contents.metadata = [indent, starting_num, OrderedList.numbering_type, separator]
         self.add_plain(kwargs["context"])
+
+
+class Table(ast.Table):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @staticmethod
+    def add_table_head(context: list) -> None:
+        table_head = TableHead()
+        context.append(table_head)
+
+    @staticmethod
+    def add_table_body(context: list) -> None:
+        table_body = TableBody()
+        context.append(table_body)
+
+    @staticmethod
+    def add_row(context: list) -> None:
+        row = Row()
+        context.append(row)
+
+    @staticmethod
+    def add_cell(context: list) -> None:
+        # needed to start first cell in table
+        cell = Cell()
+        context.append(cell)
+
+    def process_read(self, **kwargs: Unpack[ast_helpers.ProcessParams]) -> None:
+        # wywola sie przy znalezieniu startu
+        context = kwargs.get("context")
+        self.add_table_head(context)
+        self.add_row(context)
+        self.add_cell(context)
+        return
+
+    @classmethod
+    def start(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        token = kwargs["token"]
+        context = kwargs["context"]
+
+        if context and context[-1].name in ("Table", "TableHead", "TableBody", "Row", "Cell"):
+            return (None, token)
+
+        match = re.search(cls.start_pattern, token)
+        token = token[match.end() :] if match else token
+
+        return (match, token)
+
+    @classmethod
+    def end(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        token = kwargs["token"]
+        match = re.search(cls.end_pattern, token)
+        token = token[match.end() :] if match else token
+
+        return (match, token)
+
+
+class TableHead(ast.TableHead):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def end(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        context = kwargs.get("context")
+        token = kwargs.get("token")
+        if len(context[-1].contents.contents) != 2:
+            return (None, token)
+
+        match = re.search(cls.end_pattern, token)
+        token = token[match.end() :] if match else token
+
+        return (match, token)
+
+
+class TableBody(ast.TableBody):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def end(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        token = kwargs.get("token")
+
+        match = re.search(cls.end_pattern, token)
+        token = token[match.end() :] if match else token
+
+        return (match, token)
+
+
+class Row(ast.Row):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def end(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        token = kwargs.get("token")
+
+        match = re.search(cls.end_pattern, token)
+        token = token[match.end() :] if match else token
+
+        return (match, token)
+
+
+class Cell(ast.Cell):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def start(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        # cell from its regex (due to its easy to meet contition) starts only within
+        # existing table(and its components)
+        token = kwargs["token"]
+        context = kwargs["context"]
+
+        match = re.search(cls.start_pattern, token)
+        if not context or not match or context[-1].name not in ("Table", "TableHead", "TableBody", "Row"):
+            return (None, token)
+
+        match context[-1].name:
+            case "Table":
+                context[-1].add_table_body(context)
+                context[-2].add_row(context)
+            case "TableBody" | "TableHead":
+                context[-2].add_row(context)
+
+        return (match, token[match.end("c") :])
+
+    @classmethod
+    def end(cls, **kwargs: Unpack[ast_helpers.StartParams]) -> tuple[re.Match | None, str]:
+        token = kwargs["token"]
+        match = re.search(cls.end_pattern, token)
+        token = token[match.end() - 1 :] if match else token
+
+        return (match, token)
